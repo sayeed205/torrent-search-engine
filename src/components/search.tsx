6197bf5a7a3569079354f4d81695f0859d5171cf
Hitarashi <sayeed205@gmail.com>
2023-09-12 20:19:38 +0530

    add proper debounce

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import debounce from 'lodash.debounce';
+import { useRouter } from 'next/navigation';
+import { useDebounce } from 'use-debounce';
 
 import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
@@ -13,22 +13,24 @@ interface SearchProps extends React.HTMLAttributes<HTMLDivElement> {}
 export function Search({ className, ...props }: SearchProps) {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [q, setQ] = useState<string>('');
+    const [query] = useDebounce(q, 1000);
 
     const router = useRouter();
-    const searchParams = useSearchParams();
-    const que = searchParams.get('q');
+    // const searchParams = useSearchParams();
+    // const que = searchParams.get('q');
 
     useEffect(() => {
-        if (que && que !== q) {
-            setQ(que);
-        }
-    }, [que]);
+        // if (que && que !== q) {
+        //     setQ(query);
+        // }
+        router.push(`/search?q=${query}`);
+    }, [query]);
 
-    const handleSearch = debounce((q: string) => {
-        setIsLoading(true);
-        router.push(`/search?q=${q}`);
-        setIsLoading(false);
-    }, 1000);
+    // const handleSearch = debounce((q: string) => {
+    //     setIsLoading(true);
+    //     router.push(`/search?q=${q}`);
+    //     setIsLoading(false);
+    // }, 1000);
 
     return (
         <div className={cn('')} {...props}>
@@ -43,7 +45,7 @@ export function Search({ className, ...props }: SearchProps) {
                 value={q}
                 onChange={(e) => {
                     setQ(e.target.value);
-                    handleSearch(e.target.value);
+                    // handleSearch(e.target.value);
                 }}
                 className="m-auto w-[35%] top-3 left-[calc(100vw-68%)] bg-background"
             />
